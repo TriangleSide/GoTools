5988ed7bcc8615d6add3330962fc948a47c0a4bc
David Ouellette <david.o@mailbox.org>
2024-10-01 09:02:42 -0700

    Refactor the HTTP parameter tag tests to use the standard testing framework.

--- pkg/http/parameters/tags_test.go
+++ pkg/http/parameters/tags_test.go
@@ -1,158 +1,158 @@
 package parameters_test
 
 import (
-	"fmt"
+	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-
-	"github.com/TriangleSide/GoBase/pkg/ds"
 	"github.com/TriangleSide/GoBase/pkg/http/headers"
 	"github.com/TriangleSide/GoBase/pkg/http/parameters"
+	"github.com/TriangleSide/GoBase/pkg/test/assert"
 )
 
-var _ = Describe("parameter tags", func() {
-	When("a lookup key is verified against the naming convention", func() {
-		DescribeTable("naming convention test cases",
-			func(lookupKey string, expect bool) {
-				Expect(parameters.TagLookupKeyFollowsNamingConvention(lookupKey)).To(Equal(expect),
-					fmt.Sprintf("expect the validation of '%s' to be %v", lookupKey, expect))
-			},
-			// True cases.
-			Entry("", headers.ContentType, true),
-			Entry("", "a", true),
-			Entry("", "A", true),
-			Entry("", "aa", true),
-			Entry("", "aA", true),
-			Entry("", "a0", true),
-			Entry("", "a-", true),
-			Entry("", "a_", true),
-			Entry("", "aaA-_", true),
-			// False cases.
-			Entry("", "", false),
-			Entry("", "0", false),
-			Entry("", "0a", false),
-			Entry("", " name", false),
-			Entry("", "name ", false),
-			Entry("", "na me", false),
-		)
+func TestTags(t *testing.T) {
+	t.Parallel()
+
+	t.Run("lookup key name validation", func(t *testing.T) {
+		t.Parallel()
+
+		testCases := []struct {
+			lookupKey string
+			expect    bool
+		}{
+			{lookupKey: headers.ContentType, expect: true},
+			{lookupKey: "a", expect: true},
+			{lookupKey: "A", expect: true},
+			{lookupKey: "aa", expect: true},
+			{lookupKey: "aA", expect: true},
+			{lookupKey: "a0", expect: true},
+			{lookupKey: "a-", expect: true},
+			{lookupKey: "a_", expect: true},
+			{lookupKey: "aaA-_", expect: true},
+			{lookupKey: "", expect: false},
+			{lookupKey: "0", expect: false},
+			{lookupKey: "0a", expect: false},
+			{lookupKey: " name", expect: false},
+			{lookupKey: "name ", expect: false},
+			{lookupKey: "na me", expect: false},
+		}
+
+		for _, testCase := range testCases {
+			actual := parameters.TagLookupKeyFollowsNamingConvention(testCase.lookupKey)
+			assert.Equals(t, testCase.expect, actual)
+		}
 	})
 
-	When("a structs tags are extracted and validated", func() {
-		It("should succeed when the tags are properly formatted", func() {
-			type testStruct struct {
-				QueryField1  string `urlQuery:"Query1" json:"-" otherTag1:"value"`
-				QueryField2  string `urlQuery:"Query2" json:"-" otherTag1:"value"`
-				HeaderField1 string `httpHeader:"Header1" json:"-" otherTag2:"value1"`
-				HeaderField2 string `httpHeader:"Header2" json:"-" otherTag2:"value2"`
-				PathField1   string `urlPath:"Path1" json:"-" otherTag3:""`
-				PathField2   string `urlPath:"Path2" json:"-" otherTag4:"!@#$%^&*()"`
-				JSONField1   string `json:"JSON1,omitempty"`
-				JSONField2   string `json:"JSON2,omitempty"`
-			}
+	t.Run("it should succeed when the tags are properly formatted", func(t *testing.T) {
+		t.Parallel()
 
-			var err error
-			var tagToLookupKeyToFieldName ds.ReadOnlyMap[parameters.Tag, parameters.LookupKeyToFieldName]
+		type testStruct struct {
+			QueryField1  string `urlQuery:"Query1" json:"-" otherTag1:"value"`
+			QueryField2  string `urlQuery:"Query2" json:"-" otherTag1:"value"`
+			HeaderField1 string `httpHeader:"Header1" json:"-" otherTag2:"value1"`
+			HeaderField2 string `httpHeader:"Header2" json:"-" otherTag2:"value2"`
+			PathField1   string `urlPath:"Path1" json:"-" otherTag3:""`
+			PathField2   string `urlPath:"Path2" json:"-" otherTag4:"!@#$%^&*()"`
+			JSONField1   string `json:"JSON1,omitempty"`
+			JSONField2   string `json:"JSON2,omitempty"`
+		}
 
-			for i := 0; i < 3; i++ {
-				tagToLookupKeyToFieldName, err = parameters.ExtractAndValidateFieldTagLookupKeys[testStruct]()
-				Expect(err).To(Not(HaveOccurred()))
-			}
+		for i := 0; i < 3; i++ {
+			tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[testStruct]()
+			assert.NoError(t, err)
 
-			Expect(tagToLookupKeyToFieldName.Size()).To(Equal(3))
+			assert.True(t, tagToLookupKeyToFieldName.Has(parameters.QueryTag))
+			assert.True(t, tagToLookupKeyToFieldName.Has(parameters.HeaderTag))
+			assert.True(t, tagToLookupKeyToFieldName.Has(parameters.PathTag))
 
-			Expect(tagToLookupKeyToFieldName.Has(parameters.QueryTag))
-			Expect(tagToLookupKeyToFieldName.Has(parameters.HeaderTag))
-			Expect(tagToLookupKeyToFieldName.Has(parameters.PathTag))
+			assert.Equals(t, len(tagToLookupKeyToFieldName.Get(parameters.QueryTag)), 2)
+			assert.Equals(t, tagToLookupKeyToFieldName.Get(parameters.QueryTag)["query1"], "QueryField1")
+			assert.Equals(t, tagToLookupKeyToFieldName.Get(parameters.QueryTag)["query2"], "QueryField2")
 
-			Expect(tagToLookupKeyToFieldName.Get(parameters.QueryTag)).To(HaveLen(2))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.QueryTag)).To(HaveKey("query1"))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.QueryTag)["query1"]).To(Equal("QueryField1"))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.QueryTag)).To(HaveKey("query2"))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.QueryTag)["query2"]).To(Equal("QueryField2"))
+			assert.Equals(t, len(tagToLookupKeyToFieldName.Get(parameters.HeaderTag)), 2)
+			assert.Equals(t, tagToLookupKeyToFieldName.Get(parameters.HeaderTag)["header1"], "HeaderField1")
+			assert.Equals(t, tagToLookupKeyToFieldName.Get(parameters.HeaderTag)["header2"], "HeaderField2")
 
-			Expect(tagToLookupKeyToFieldName.Get(parameters.HeaderTag)).To(HaveLen(2))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.HeaderTag)).To(HaveKey("header1"))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.HeaderTag)["header1"]).To(Equal("HeaderField1"))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.HeaderTag)).To(HaveKey("header2"))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.HeaderTag)["header2"]).To(Equal("HeaderField2"))
+			assert.Equals(t, len(tagToLookupKeyToFieldName.Get(parameters.PathTag)), 2)
+			assert.Equals(t, tagToLookupKeyToFieldName.Get(parameters.PathTag)["Path1"], "PathField1")
+			assert.Equals(t, tagToLookupKeyToFieldName.Get(parameters.PathTag)["Path2"], "PathField2")
+		}
+	})
 
-			Expect(tagToLookupKeyToFieldName.Get(parameters.PathTag)).To(HaveLen(2))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.PathTag)).To(HaveKey("Path1"))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.PathTag)["Path1"]).To(Equal("PathField1"))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.PathTag)).To(HaveKey("Path2"))
-			Expect(tagToLookupKeyToFieldName.Get(parameters.PathTag)["Path2"]).To(Equal("PathField2"))
-		})
+	t.Run("it should fail when validating a struct that has two fields with the same tag", func(t *testing.T) {
+		t.Parallel()
+		type testStruct struct {
+			Field1 string `urlQuery:"QueryField" json:"-"`
+			Field2 int    `urlQuery:"QueryField" json:"-"`
+		}
+		tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[testStruct]()
+		assert.Error(t, err)
+		assert.Nil(t, tagToLookupKeyToFieldName)
+	})
 
-		It("should fail when validating a struct that has two fields with the same tag", func() {
-			tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[struct {
-				Field1 string `urlQuery:"QueryField" json:"-"`
-				Field2 int    `urlQuery:"QueryField" json:"-"`
-			}]()
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(ContainSubstring("is not unique"))
-			Expect(tagToLookupKeyToFieldName).To(BeNil())
-		})
+	t.Run("it should fail when validating a struct that has two fields with the same tag in different cases", func(t *testing.T) {
+		t.Parallel()
+		type testStruct struct {
+			Field1 string `urlQuery:"QueryField" json:"-"`
+			Field2 int    `urlQuery:"qUeRyfIeLd" json:"-"`
+		}
+		tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[testStruct]()
+		assert.Error(t, err)
+		assert.Nil(t, tagToLookupKeyToFieldName)
+	})
 
-		It("should fail when validating a struct that has two fields with the same tag in different cases", func() {
-			tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[struct {
-				Field1 string `urlQuery:"QueryField" json:"-"`
-				Field2 int    `urlQuery:"qUeRyfIeLd" json:"-"`
-			}]()
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(ContainSubstring("is not unique"))
-			Expect(tagToLookupKeyToFieldName).To(BeNil())
-		})
+	t.Run("it should fail when validating a struct that has overlapping tags", func(t *testing.T) {
+		t.Parallel()
+		type testStruct struct {
+			Field string `urlQuery:"QueryField" httpHeader:"HeaderField" json:"-"`
+		}
+		tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[testStruct]()
+		assert.Error(t, err)
+		assert.Nil(t, tagToLookupKeyToFieldName)
+	})
 
-		It("should fail when validating a struct that has overlapping tags", func() {
-			tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[struct {
-				Field string `urlQuery:"QueryField" httpHeader:"HeaderField" json:"-"`
-			}]()
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(ContainSubstring("there can only be one encoding tag on the field 'Field'"))
-			Expect(tagToLookupKeyToFieldName).To(BeNil())
-		})
+	t.Run("it should fail when validating a struct that has no accompanying json tag", func(t *testing.T) {
+		t.Parallel()
+		type testStruct struct {
+			Field string `urlQuery:"QueryField"`
+		}
+		tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[testStruct]()
+		assert.Error(t, err)
+		assert.Nil(t, tagToLookupKeyToFieldName)
+	})
 
-		It("should fail when validating a struct that has no accompanying json tag", func() {
-			tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[struct {
-				Field string `urlQuery:"QueryField"`
-			}]()
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(ContainSubstring("struct field 'Field' with tag 'urlQuery' must have accompanying tag json:\"-\""))
-			Expect(tagToLookupKeyToFieldName).To(BeNil())
-		})
+	t.Run("it should fail when validating a struct that has an accompanying json tag with the wrong format", func(t *testing.T) {
+		t.Parallel()
+		type testStruct struct {
+			Field string `httpHeader:"QueryField" json:"notRight"`
+		}
+		tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[testStruct]()
+		assert.Error(t, err)
+		assert.Nil(t, tagToLookupKeyToFieldName)
+	})
 
-		It("should fail when validating a struct that has an accompanying json tag with the wrong format", func() {
-			tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[struct {
-				Field string `httpHeader:"QueryField" json:"notRight"`
-			}]()
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(ContainSubstring("struct field 'Field' with tag 'httpHeader' must have accompanying tag json:\"-\""))
-			Expect(tagToLookupKeyToFieldName).To(BeNil())
-		})
+	t.Run("it should fail when validating a struct that has a tag that is empty", func(t *testing.T) {
+		t.Parallel()
+		type testStruct struct {
+			Field string `urlPath:"" json:"-"`
+		}
+		tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[testStruct]()
+		assert.Error(t, err)
+		assert.Nil(t, tagToLookupKeyToFieldName)
+	})
 
-		It("should fail when validating a struct that has a tag that is empty", func() {
-			tagToLookupKeyToFieldName, err := parameters.ExtractAndValidateFieldTagLookupKeys[struct {
-				Field string `urlPath:"" json:"-"`
-			}]()
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(ContainSubstring("tag 'urlPath' with lookup key '' must adhere to the naming convention"))
-			Expect(tagToLookupKeyToFieldName).To(BeNil())
-		})
-
-		It("should panic when the generic isn't a struct", func() {
-			Expect(func() {
-				_, _ = parameters.ExtractAndValidateFieldTagLookupKeys[string]()
-			}).Should(PanicWith(ContainSubstring("type must be a struct")))
-		})
-
-		It("should fail when the generic is a struct pointer", func() {
-			type parameterStruct struct {
-				Field string `urlQuery:"" json:"-"`
-			}
-			Expect(func() {
-				_, _ = parameters.ExtractAndValidateFieldTagLookupKeys[*parameterStruct]()
-			}).Should(PanicWith(ContainSubstring("type must be a struct")))
+	t.Run("it should panic when the generic isn't a struct", func(t *testing.T) {
+		t.Parallel()
+		assert.Panic(t, func() {
+			_, _ = parameters.ExtractAndValidateFieldTagLookupKeys[string]()
 		})
 	})
-})
+
+	t.Run("it should panic when the generic is a struct pointer", func(t *testing.T) {
+		t.Parallel()
+		type parameterStruct struct {
+			Field string `urlQuery:"" json:"-"`
+		}
+		assert.Panic(t, func() {
+			_, _ = parameters.ExtractAndValidateFieldTagLookupKeys[*parameterStruct]()
+		})
+	})
+}
