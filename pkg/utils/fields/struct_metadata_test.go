a1c3c4d93dc1af2c4a39126a07f285be6b66a767
David Ouellette <david.o@mailbox.org>
2024-10-07 13:32:28 -0300

    Refactor the struct field metadata utility tests to use the standard testing framework.

--- pkg/utils/fields/struct_metadata_test.go
+++ pkg/utils/fields/struct_metadata_test.go
@@ -1,113 +1,97 @@
 package fields_test
 
 import (
+	"fmt"
 	"reflect"
+	"sync"
+	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-
-	reflectutils "github.com/TriangleSide/GoBase/pkg/utils/fields"
+	"github.com/TriangleSide/GoBase/pkg/test/assert"
+	"github.com/TriangleSide/GoBase/pkg/utils/fields"
 )
 
-var _ = Describe("struct field metadata", func() {
-	It("should panic if the type is not a struct", func() {
-		Expect(func() {
-			_ = reflectutils.StructMetadata[int]()
-		}).Should(PanicWith(ContainSubstring("type must be a struct")))
+func TestStructMetadata(t *testing.T) {
+	t.Parallel()
+
+	t.Run("when the type is not a struct it should panic", func(t *testing.T) {
+		assert.PanicPart(t, func() {
+			_ = fields.StructMetadata[int]()
+		}, "type must be a struct")
 	})
 
-	It("should panic if the type is a pointer to a struct", func() {
-		Expect(func() {
-			_ = reflectutils.StructMetadata[*struct{}]()
-		}).Should(PanicWith(ContainSubstring("type must be a struct")))
+	t.Run("when the type is a pointer to a struct it should panic", func(t *testing.T) {
+		assert.PanicPart(t, func() {
+			_ = fields.StructMetadata[*struct{}]()
+		}, "type must be a struct")
 	})
 
-	It("should return an empty map for an empty struct", func() {
-		metadata := reflectutils.StructMetadata[struct{}]()
-		Expect(metadata.Size()).To(Equal(0))
+	t.Run("when the struct is empty it should return an empty map", func(t *testing.T) {
+		metadata := fields.StructMetadata[struct{}]()
+		assert.Equals(t, metadata.Size(), 0)
 	})
 
-	It("should succeed if done many times on the same struct type", func() {
-		type testStruct struct{}
-		for i := 0; i < 2; i++ {
-			metadata := reflectutils.StructMetadata[testStruct]()
-			Expect(metadata.Size()).To(Equal(0))
-		}
-	})
-
-	When("when a struct has a string field called Value and no tag", func() {
+	t.Run("when a struct has a string field called Value and no tag it should return the field name and its type without metadata", func(t *testing.T) {
 		type testStruct struct {
 			Value string
 		}
-
-		It("should return the field name and its type with no metadata", func() {
-			metadata := reflectutils.StructMetadata[testStruct]()
-			Expect(metadata.Size()).To(Equal(1))
-			Expect(metadata.Has("Value")).To(BeTrue())
-			Expect(metadata.Get("Value").Type.Kind()).To(Equal(reflect.String))
-			Expect(metadata.Get("Value").Tags).To(BeEmpty())
-			Expect(metadata.Get("Value").Anonymous).To(BeEmpty())
-		})
+		metadata := fields.StructMetadata[testStruct]()
+		assert.Equals(t, metadata.Size(), 1)
+		valueField := metadata.Get("Value")
+		assert.True(t, metadata.Has("Value"))
+		assert.Equals(t, valueField.Type.Kind(), reflect.String)
+		assert.Equals(t, len(valueField.Tags), 0)
+		assert.Equals(t, len(valueField.Anonymous), 0)
 	})
 
-	When("when a struct has a string field called Value and a tag", func() {
+	t.Run("when a struct has a string field called Value and a tag it should return the field name and its type with the tag metadata", func(t *testing.T) {
 		type testStruct struct {
 			Value int `key:"Value"`
 		}
-
-		It("should return the field name and its type with the tag metadata", func() {
-			metadata := reflectutils.StructMetadata[testStruct]()
-			Expect(metadata.Size()).To(Equal(1))
-			Expect(metadata.Has("Value")).To(BeTrue())
-			Expect(metadata.Get("Value").Type.Kind()).To(Equal(reflect.Int))
-			Expect(metadata.Get("Value").Tags).To(HaveLen(1))
-			Expect(metadata.Get("Value").Tags).To(HaveKeyWithValue("key", "Value"))
-			Expect(metadata.Get("Value").Anonymous).To(BeEmpty())
-		})
+		metadata := fields.StructMetadata[testStruct]()
+		assert.Equals(t, metadata.Size(), 1)
+		valueField := metadata.Get("Value")
+		assert.True(t, metadata.Has("Value"))
+		assert.Equals(t, valueField.Type.Kind(), reflect.Int)
+		assert.Equals(t, valueField.Tags["key"], "Value")
+		assert.Equals(t, len(valueField.Anonymous), 0)
 	})
 
-	When("when a struct has a string field called Value and a tag with multiple fields", func() {
+	t.Run("when a struct has a string field called Value and tags with multiple fields it should return the field name and its type with the tags metadata", func(t *testing.T) {
 		type testStruct struct {
 			Value float32 `key1:"Value1" key2:"Value2"`
 		}
-
-		It("should return the field name and its type with the tags metadata", func() {
-			metadata := reflectutils.StructMetadata[testStruct]()
-			Expect(metadata.Size()).To(Equal(1))
-			Expect(metadata.Has("Value")).To(BeTrue())
-			Expect(metadata.Get("Value").Type.Kind()).To(Equal(reflect.Float32))
-			Expect(metadata.Get("Value").Tags).To(HaveLen(2))
-			Expect(metadata.Get("Value").Tags).To(HaveKeyWithValue("key1", "Value1"))
-			Expect(metadata.Get("Value").Tags).To(HaveKeyWithValue("key2", "Value2"))
-			Expect(metadata.Get("Value").Anonymous).To(BeEmpty())
-		})
+		metadata := fields.StructMetadata[testStruct]()
+		assert.Equals(t, metadata.Size(), 1)
+		valueField := metadata.Get("Value")
+		assert.True(t, metadata.Has("Value"))
+		assert.Equals(t, valueField.Type.Kind(), reflect.Float32)
+		assert.Equals(t, valueField.Tags["key1"], "Value1")
+		assert.Equals(t, valueField.Tags["key2"], "Value2")
+		assert.Equals(t, len(valueField.Anonymous), 0)
 	})
 
-	When("when a struct has multiple fields with tags with multiple fields", func() {
+	t.Run("when a struct has multiple fields with tags, it should return their field names and type with their tags metadata", func(t *testing.T) {
 		type testStruct struct {
 			Value1 string `key2:"Value3" key4:"Value5"`
 			Value6 string `key7:"Value8" key9:"Value10"`
 		}
 
-		It("should return the field names and their type with their tags metadata", func() {
-			metadata := reflectutils.StructMetadata[testStruct]()
-			Expect(metadata.Size()).To(Equal(2))
-			Expect(metadata.Has("Value1")).To(BeTrue())
-			Expect(metadata.Get("Value1").Type.Kind()).To(Equal(reflect.String))
-			Expect(metadata.Get("Value1").Tags).To(HaveLen(2))
-			Expect(metadata.Get("Value1").Tags).To(HaveKeyWithValue("key2", "Value3"))
-			Expect(metadata.Get("Value1").Tags).To(HaveKeyWithValue("key4", "Value5"))
-			Expect(metadata.Get("Value1").Anonymous).To(BeEmpty())
-			Expect(metadata.Has("Value6")).To(BeTrue())
-			Expect(metadata.Get("Value6").Type.Kind()).To(Equal(reflect.String))
-			Expect(metadata.Get("Value6").Tags).To(HaveLen(2))
-			Expect(metadata.Get("Value6").Tags).To(HaveKeyWithValue("key7", "Value8"))
-			Expect(metadata.Get("Value6").Tags).To(HaveKeyWithValue("key9", "Value10"))
-			Expect(metadata.Get("Value6").Anonymous).To(BeEmpty())
-		})
+		metadata := fields.StructMetadata[testStruct]()
+		assert.Equals(t, metadata.Size(), 2)
+		value1Field := metadata.Get("Value1")
+		assert.True(t, metadata.Has("Value1"))
+		assert.Equals(t, value1Field.Type.Kind(), reflect.String)
+		assert.Equals(t, value1Field.Tags["key2"], "Value3")
+		assert.Equals(t, value1Field.Tags["key4"], "Value5")
+
+		value6Field := metadata.Get("Value6")
+		assert.True(t, metadata.Has("Value6"))
+		assert.Equals(t, value6Field.Type.Kind(), reflect.String)
+		assert.Equals(t, value6Field.Tags["key7"], "Value8")
+		assert.Equals(t, value6Field.Tags["key9"], "Value10")
 	})
 
-	When("when a struct has nested structs, all with fields and a tags", func() {
+	t.Run("when a struct has nested anonymous structs with fields and tags it should include the anonymous structs fields", func(t *testing.T) {
 		type deepStruct struct {
 			DeepField string `key:"Deep"`
 		}
@@ -127,37 +111,34 @@ var _ = Describe("struct field metadata", func() {
 			OuterField string `key:"Outer"`
 		}
 
-		It("should return the anonymous structs fields", func() {
-			metadata := reflectutils.StructMetadata[outerStruct]()
-			Expect(metadata.Size()).To(Equal(4))
-			Expect(metadata.Has("DeepField")).To(BeTrue())
-			Expect(metadata.Get("DeepField").Type.Kind()).To(Equal(reflect.String))
-			Expect(metadata.Get("DeepField").Tags).To(HaveLen(1))
-			Expect(metadata.Get("DeepField").Tags).Should(HaveKeyWithValue("key", "Deep"))
-			Expect(metadata.Get("DeepField").Anonymous).To(HaveLen(2))
-			Expect(metadata.Get("DeepField").Anonymous[0]).To(Equal("embeddedStruct1"))
-			Expect(metadata.Get("DeepField").Anonymous[1]).To(Equal("deepStruct"))
-			Expect(metadata.Has("EmbeddedField1")).To(BeTrue())
-			Expect(metadata.Get("EmbeddedField1").Type.Kind()).To(Equal(reflect.String))
-			Expect(metadata.Get("EmbeddedField1").Tags).To(HaveLen(1))
-			Expect(metadata.Get("EmbeddedField1").Tags).To(HaveKeyWithValue("key", "Embedded1"))
-			Expect(metadata.Get("EmbeddedField1").Anonymous).To(HaveLen(1))
-			Expect(metadata.Get("EmbeddedField1").Anonymous[0]).To(Equal("embeddedStruct1"))
-			Expect(metadata.Has("EmbeddedField2")).To(BeTrue())
-			Expect(metadata.Get("EmbeddedField2").Type.Kind()).To(Equal(reflect.String))
-			Expect(metadata.Get("EmbeddedField2").Tags).To(HaveLen(1))
-			Expect(metadata.Get("EmbeddedField2").Tags).To(HaveKeyWithValue("key", "Embedded2"))
-			Expect(metadata.Get("EmbeddedField2").Anonymous).To(HaveLen(1))
-			Expect(metadata.Get("EmbeddedField2").Anonymous[0]).To(Equal("embeddedStruct2"))
-			Expect(metadata.Has("OuterField")).To(BeTrue())
-			Expect(metadata.Get("OuterField").Type.Kind()).To(Equal(reflect.String))
-			Expect(metadata.Get("OuterField").Tags).To(HaveLen(1))
-			Expect(metadata.Get("OuterField").Tags).To(HaveKeyWithValue("key", "Outer"))
-			Expect(metadata.Get("OuterField").Anonymous).To(BeEmpty())
-		})
+		metadata := fields.StructMetadata[outerStruct]()
+		assert.Equals(t, metadata.Size(), 4)
+		deepField := metadata.Get("DeepField")
+		assert.True(t, metadata.Has("DeepField"))
+		assert.Equals(t, deepField.Type.Kind(), reflect.String)
+		assert.Equals(t, deepField.Tags["key"], "Deep")
+		assert.Equals(t, deepField.Anonymous, []string{"embeddedStruct1", "deepStruct"})
+
+		embeddedField1 := metadata.Get("EmbeddedField1")
+		assert.True(t, metadata.Has("EmbeddedField1"))
+		assert.Equals(t, embeddedField1.Type.Kind(), reflect.String)
+		assert.Equals(t, embeddedField1.Tags["key"], "Embedded1")
+		assert.Equals(t, embeddedField1.Anonymous, []string{"embeddedStruct1"})
+
+		embeddedField2 := metadata.Get("EmbeddedField2")
+		assert.True(t, metadata.Has("EmbeddedField2"))
+		assert.Equals(t, embeddedField2.Type.Kind(), reflect.String)
+		assert.Equals(t, embeddedField2.Tags["key"], "Embedded2")
+		assert.Equals(t, embeddedField2.Anonymous, []string{"embeddedStruct2"})
+
+		outerField := metadata.Get("OuterField")
+		assert.True(t, metadata.Has("OuterField"))
+		assert.Equals(t, outerField.Type.Kind(), reflect.String)
+		assert.Equals(t, outerField.Tags["key"], "Outer")
+		assert.Equals(t, len(outerField.Anonymous), 0)
 	})
 
-	When("when a struct has a nested struct, both with the same field name", func() {
+	t.Run("when a struct and a nested struct both have fields with the same name it should panic", func(t *testing.T) {
 		type embeddedStruct struct {
 			Field string
 		}
@@ -167,10 +148,37 @@ var _ = Describe("struct field metadata", func() {
 			Field string
 		}
 
-		It("should panic", func() {
-			Expect(func() {
-				_ = reflectutils.StructMetadata[outerStruct]()
-			}).Should(PanicWith(ContainSubstring("field Field is ambiguous")))
-		})
+		assert.PanicPart(t, func() {
+			_ = fields.StructMetadata[outerStruct]()
+		}, fmt.Sprintf("field Field is ambiguous"))
 	})
-})
+
+	t.Run("when StructMedata is called concurrently is should have no errors", func(t *testing.T) {
+		type testStruct struct {
+			Value float32 `key1:"Value1" key2:"Value2"`
+		}
+		const threadCount = 8
+		const loopCount = 1000
+		wg := sync.WaitGroup{}
+		waitChan := make(chan struct{})
+		for i := 0; i < threadCount; i++ {
+			wg.Add(1)
+			go func() {
+				<-waitChan
+				for k := 0; k < loopCount; k++ {
+					metadata := fields.StructMetadata[testStruct]()
+					assert.Equals(t, metadata.Size(), 1)
+					valueField := metadata.Get("Value")
+					assert.True(t, metadata.Has("Value"))
+					assert.Equals(t, valueField.Type.Kind(), reflect.Float32)
+					assert.Equals(t, valueField.Tags["key1"], "Value1")
+					assert.Equals(t, valueField.Tags["key2"], "Value2")
+					assert.Equals(t, len(valueField.Anonymous), 0)
+				}
+				wg.Done()
+			}()
+		}
+		close(waitChan)
+		wg.Wait()
+	})
+}
