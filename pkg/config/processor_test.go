9d609e87c87634905bbbc03ff722b4fdfb5ccb2d
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2025-06-04 16:42:09 -0700

    Refactor the config processor files.

--- pkg/config/processor_test.go
+++ pkg/config/processor_test.go
@@ -151,23 +151,6 @@ func TestConfigProcessor(t *testing.T) {
 		assert.Equals(t, conf.Value, "custom")
 	})
 
-	t.Run("when a custom processor is re-registered it should panic", func(t *testing.T) {
-		config.MustRegisterProcessor("TWICE", func(fieldName string, _ *structs.FieldMetadata, _ config.Options) (string, bool, error) {
-			return "", true, nil
-		})
-		assert.PanicExact(t, func() {
-			config.MustRegisterProcessor("TWICE", func(fieldName string, _ *structs.FieldMetadata, _ config.Options) (string, bool, error) {
-				return "", true, nil
-			})
-		}, "Processor with name \"TWICE\" already registered.")
-	})
-
-	t.Run("when a custom processor is registered with a nil SourceFunc it should panic", func(t *testing.T) {
-		assert.PanicExact(t, func() {
-			config.MustRegisterProcessor("TWICE", nil)
-		}, "Must register a non-nil SourceFunc for the TWICE configuration processor.")
-	})
-
 	t.Run("multiple processors should be able to be used", func(t *testing.T) {
 		type testStruct struct {
 			EnvValue   string `config:"ENV"`
---
+++ pkg/config/registry.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/TriangleSide/GoTools/pkg/structs"
+)
+
+var (
+	// processors is a map of ProcessorTag value to how the values are fetched.
+	processors = map[string]SourceFunc{}
+)
+
+// SourceFunc fetches a configuration value for a field. It should return the value and whether it was found.
+type SourceFunc func(fieldName string, fieldMetadata *structs.FieldMetadata, cfg Options) (string, bool, error)
+
+// MustRegisterProcessor registers a SourceFunc for a given processor name.
+func MustRegisterProcessor(name string, fn SourceFunc) {
+	if fn == nil {
+		panic(fmt.Sprintf("Must register a non-nil SourceFunc for the %s configuration processor.", name))
+	}
+	if _, exists := processors[name]; exists {
+		panic(fmt.Sprintf("Processor with name %q already registered.", name))
+	}
+	processors[name] = fn
+}
