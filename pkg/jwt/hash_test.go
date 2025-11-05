6e68ddcc81b86e35f3d06be35947262bedff3c16
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2025-11-05 12:36:34 -0800

    Run hash tests in parallel

--- pkg/jwt/hash_test.go
+++ pkg/jwt/hash_test.go
@@ -22,7 +22,9 @@ func (f *failingHash) Size() int                         { return 1 }
 func (f *failingHash) BlockSize() int                    { return 1 }
 
 func TestHash(t *testing.T) {
+	t.Parallel()
 	t.Run("it should hash and verify data", func(t *testing.T) {
+		t.Parallel()
 		hashed, err := hashData("payload", "secret", sha256.New)
 		assert.NoError(t, err)
 		ok, err := verifyHash(hashed, "payload", "secret", sha256.New)
@@ -31,6 +33,7 @@ func TestHash(t *testing.T) {
 	})
 
 	t.Run("when hashes do not match it should return false", func(t *testing.T) {
+		t.Parallel()
 		hashed, err := hashData("payload", "secret", sha256.New)
 		assert.NoError(t, err)
 		ok, err := verifyHash(hashed+"extra", "payload", "secret", sha256.New)
@@ -39,6 +42,7 @@ func TestHash(t *testing.T) {
 	})
 
 	t.Run("it should use a custom hash provider", func(t *testing.T) {
+		t.Parallel()
 		hashed, err := hashData("payload", "secret", sha512.New)
 		assert.NoError(t, err)
 		ok, err := verifyHash(hashed, "payload", "secret", sha512.New)
@@ -47,24 +51,28 @@ func TestHash(t *testing.T) {
 	})
 
 	t.Run("when the provider is nil it should return an error", func(t *testing.T) {
+		t.Parallel()
 		h, err := hashData("payload", "secret", nil)
 		assert.ErrorPart(t, err, "hash provider cannot be nil")
 		assert.Equals(t, h, "")
 	})
 
 	t.Run("when the hash provider returns an error on write it should return an error", func(t *testing.T) {
+		t.Parallel()
 		h, err := hashData("payload", "secret", func() hash.Hash { return &failingHash{} })
 		assert.ErrorPart(t, err, "failed to write data to hash")
 		assert.Equals(t, h, "")
 	})
 
 	t.Run("when verify uses a failing hash provider it should return an error", func(t *testing.T) {
+		t.Parallel()
 		ok, err := verifyHash("", "payload", "secret", func() hash.Hash { return &failingHash{} })
 		assert.ErrorPart(t, err, "failed to write data to hash")
 		assert.False(t, ok)
 	})
 
 	t.Run("when verify is called with a nil provider it should return an error", func(t *testing.T) {
+		t.Parallel()
 		ok, err := verifyHash("", "payload", "secret", nil)
 		assert.ErrorPart(t, err, "hash provider cannot be nil")
 		assert.False(t, ok)
