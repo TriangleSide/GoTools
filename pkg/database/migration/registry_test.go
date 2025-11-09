db289b872c4cb7017e485e4cf6554d6ab0b29653
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2025-11-08 20:20:14 -0800

    Remove the global database migration registry and put it in a struct instead.

--- pkg/database/migration/registry_test.go
+++ pkg/database/migration/registry_test.go
@@ -1,39 +1,38 @@
-package migration
+package migration_test
 
 import (
 	"context"
 	"fmt"
-	"sync/atomic"
 	"testing"
 
+	"github.com/TriangleSide/GoTools/pkg/database/migration"
 	"github.com/TriangleSide/GoTools/pkg/test/assert"
 )
 
 func TestRegistry(t *testing.T) {
-	registry.Clear()
-
-	order := atomic.Int32{}
-	order.Store(0)
+	t.Parallel()
 
 	t.Run("when an order is registered twice it should fail", func(t *testing.T) {
 		t.Parallel()
-		registrationOrder := Order(order.Add(1))
-		reggy := &Registration{
+		reg := migration.NewRegistry()
+		registrationOrder := migration.Order(1)
+		reggy := &migration.Registration{
 			Order:   registrationOrder,
 			Migrate: func(ctx context.Context) error { return nil },
 			Enabled: true,
 		}
-		MustRegister(reggy)
+		reg.MustRegister(reggy)
 		assert.PanicPart(t, func() {
-			MustRegister(reggy)
+			reg.MustRegister(reggy)
 		}, fmt.Sprintf("order %d already exists", registrationOrder))
 	})
 
 	t.Run("when the registration fails the validation it should panic", func(t *testing.T) {
 		t.Parallel()
-		registrationOrder := Order(order.Add(1))
+		reg := migration.NewRegistry()
+		registrationOrder := migration.Order(1)
 		assert.PanicPart(t, func() {
-			MustRegister(&Registration{
+			reg.MustRegister(&migration.Registration{
 				Order:   registrationOrder,
 				Migrate: nil,
 				Enabled: true,
@@ -41,30 +40,21 @@ func TestRegistry(t *testing.T) {
 		}, "Validation failed for registration")
 	})
 
-	t.Run("when orderedRegistrations is called the registration should be in order", func(t *testing.T) {
+	t.Run("when OrderedRegistrations is called the registration should be in order", func(t *testing.T) {
 		t.Parallel()
+		reg := migration.NewRegistry()
 		const count = 32
-		for range count {
-			registrationOrder := Order(order.Add(1))
-			MustRegister(&Registration{
+		for i := range count {
+			registrationOrder := migration.Order(count - i)
+			reg.MustRegister(&migration.Registration{
 				Order:   registrationOrder,
 				Migrate: func(ctx context.Context) error { return nil },
 				Enabled: true,
 			})
 		}
-		ordered := orderedRegistrations()
+		ordered := reg.OrderedRegistrations()
 		for i := 1; i < len(ordered); i++ {
 			assert.True(t, ordered[i-1].Order < ordered[i].Order)
 		}
-	})
-
-	t.Run("when a non-Registration type is stored it should panic", func(t *testing.T) {
-		t.Cleanup(func() {
-			registry.Clear()
-		})
-		registry.Store(Order(1), "invalid registration type")
-		assert.PanicPart(t, func() {
-			_ = orderedRegistrations()
-		}, fmt.Sprintf("order %d was not a *Registration", Order(1)))
 	})
 }
