a99f432cfde5dfbe4892c06407c35b985ff37726
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2025-11-09 19:24:52 -0800

    Fix testpackage linter error.

--- pkg/logger/formatter_test.go
+++ pkg/logger/formatter_test.go
@@ -1,38 +1,53 @@
-package logger
+package logger_test
 
 import (
+	"bytes"
 	"context"
+	"os"
 	"testing"
 
+	"github.com/TriangleSide/GoTools/pkg/logger"
 	"github.com/TriangleSide/GoTools/pkg/test/assert"
 )
 
 func TestFormatter(t *testing.T) {
+	recordOutput := func(t *testing.T) *bytes.Buffer {
+		t.Helper()
+		var output bytes.Buffer
+		logger.SetOutput(&output)
+		t.Cleanup(func() {
+			logger.SetOutput(os.Stdout)
+		})
+		return &output
+	}
+
 	t.Run("when the fields are nil it should format without fields", func(t *testing.T) {
-		msg := formatLog(nil, "test message")
-		assert.Contains(t, msg, "test message")
+		buf := recordOutput(t)
+		logger.Info("test message")
+		assert.Contains(t, buf.String(), "test message")
 	})
 
 	t.Run("when fields are formatted it should include fields", func(t *testing.T) {
+		buf := recordOutput(t)
 		ctx := context.Background()
-		testLogger := AddFields(&ctx, map[string]any{
+		log := logger.AddFields(&ctx, map[string]any{
 			"key1": "value1",
 			"key2": 2,
 		})
-		testEntry := testLogger.(*entry)
-		msg := formatLog(testEntry.fields, "test message")
-		assert.Contains(t, msg, "key1=value1")
-		assert.Contains(t, msg, "key2=2")
+		log.Info("test message")
+		assert.Contains(t, buf.String(), "key1=value1")
+		assert.Contains(t, buf.String(), "key2=2")
 	})
 
 	t.Run("when SetFormatter is called it should set custom formatter", func(t *testing.T) {
 		t.Cleanup(func() {
-			SetFormatter(DefaultFormatter)
+			logger.SetFormatter(logger.DefaultFormatter)
 		})
-		SetFormatter(func(fields map[string]any, msg string) string {
+		logger.SetFormatter(func(fields map[string]any, msg string) string {
 			return "custom: " + msg
 		})
-		msg := formatLog(nil, "test message")
-		assert.Contains(t, msg, "custom: test message")
+		buf := recordOutput(t)
+		logger.Info("test message")
+		assert.Contains(t, buf.String(), "custom: test message")
 	})
 }
