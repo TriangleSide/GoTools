00c53e3ca59cda7b71256130b69e4fe9841d8338
David Ouellette <david.o@mailbox.org>
2025-11-14 20:32:57 -0800

    Fix lint in the logger tests.

--- pkg/logger/config_test.go
+++ pkg/logger/config_test.go
@@ -1,4 +1,4 @@
-package logger
+package logger_test
 
 import (
 	"bytes"
@@ -8,28 +8,27 @@ import (
 	"os"
 	"testing"
 
+	"github.com/TriangleSide/GoTools/pkg/logger"
 	"github.com/TriangleSide/GoTools/pkg/test/assert"
 )
 
 func TestMustConfigure(t *testing.T) {
-	t.Cleanup(func() {
-		SetOutput(os.Stdout)
-		SetLevel(LevelInfo)
-	})
-
 	t.Run("when the config provider succeeds it sets the logger level", func(t *testing.T) {
-		MustConfigure(WithConfigProvider(func() (*Config, error) {
-			return &Config{
+		t.Cleanup(func() {
+			logger.SetLevel(logger.LevelInfo)
+		})
+		logger.MustConfigure(logger.WithConfigProvider(func() (*logger.Config, error) {
+			return &logger.Config{
 				LogLevel: "debug",
 			}, nil
 		}))
-		assert.Equals(t, appLogLevel, LevelDebug)
+		assert.Equals(t, logger.GetLevel(), logger.LevelDebug)
 	})
 
 	t.Run("when the level is incorrect it should panic", func(t *testing.T) {
 		assert.PanicExact(t, func() {
-			MustConfigure(WithConfigProvider(func() (*Config, error) {
-				return &Config{
+			logger.MustConfigure(logger.WithConfigProvider(func() (*logger.Config, error) {
+				return &logger.Config{
 					LogLevel: "incorrect",
 				}, nil
 			}))
@@ -38,32 +37,38 @@ func TestMustConfigure(t *testing.T) {
 
 	t.Run("when the config provider fails it should panic", func(t *testing.T) {
 		assert.PanicExact(t, func() {
-			MustConfigure(WithConfigProvider(func() (*Config, error) {
+			logger.MustConfigure(logger.WithConfigProvider(func() (*logger.Config, error) {
 				return nil, errors.New("config error")
 			}))
 		}, "Failed to get logger config (config error).")
 	})
 
 	t.Run("when the output provider succeeds does not panic", func(t *testing.T) {
+		t.Cleanup(func() {
+			logger.SetOutput(os.Stdout)
+		})
 		var outputBuffer bytes.Buffer
-		MustConfigure(WithOutputProvider(func() (io.Writer, error) {
+		logger.MustConfigure(logger.WithOutputProvider(func() (io.Writer, error) {
 			return &outputBuffer, nil
 		}))
-		Error(context.Background(), "test message")
+		logger.Error(context.Background(), "test message")
 		assert.Contains(t, outputBuffer.String(), "test message")
 	})
 
 	t.Run("when the output provider fails it should panic", func(t *testing.T) {
 		assert.PanicExact(t, func() {
-			MustConfigure(WithOutputProvider(func() (io.Writer, error) {
+			logger.MustConfigure(logger.WithOutputProvider(func() (io.Writer, error) {
 				return nil, errors.New("output error")
 			}))
 		}, "Failed to get logger output (output error).")
 	})
 
 	t.Run("when the defaults are used it should set the defaults", func(t *testing.T) {
-		SetLevel(LevelTrace)
-		MustConfigure()
-		assert.Equals(t, appLogLevel, LevelInfo)
+		t.Cleanup(func() {
+			logger.SetLevel(logger.LevelInfo)
+		})
+		logger.SetLevel(logger.LevelTrace)
+		logger.MustConfigure()
+		assert.Equals(t, logger.GetLevel(), logger.LevelInfo)
 	})
 }
