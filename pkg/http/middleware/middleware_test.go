5733e1dcd580e9d5dd3bee92a2b90ba3e4bf7e18
David Ouellette <david.o@mailbox.org>
2025-12-02 19:30:17 -0800

    Improve HTTP middleware and parameters error handling.

--- pkg/http/middleware/middleware_test.go
+++ pkg/http/middleware/middleware_test.go
@@ -65,4 +65,74 @@ func TestHTTPMiddleware(t *testing.T) {
 		assert.Equals(t, invocations[1], "second")
 		assert.Equals(t, invocations[2], "handler")
 	})
+
+	t.Run("when the final handler is nil it should panic", func(t *testing.T) {
+		t.Parallel()
+
+		defer func() {
+			r := recover()
+			assert.NotNil(t, r)
+			assert.Equals(t, r, "the final handler cannot be nil")
+		}()
+		middleware.CreateChain(nil, nil)
+	})
+
+	t.Run("when the middleware list contains nil entries it should skip them", func(t *testing.T) {
+		t.Parallel()
+
+		invocations := []string{}
+		mwList := []middleware.Middleware{
+			func(next http.HandlerFunc) http.HandlerFunc {
+				return func(writer http.ResponseWriter, request *http.Request) {
+					invocations = append(invocations, "first")
+					next(writer, request)
+				}
+			},
+			nil,
+			func(next http.HandlerFunc) http.HandlerFunc {
+				return func(writer http.ResponseWriter, request *http.Request) {
+					invocations = append(invocations, "second")
+					next(writer, request)
+				}
+			},
+		}
+		handler := func(w http.ResponseWriter, req *http.Request) {
+			invocations = append(invocations, "handler")
+		}
+		mwChain := middleware.CreateChain(mwList, handler)
+		mwChain.ServeHTTP(httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/", nil))
+
+		assert.Equals(t, len(invocations), 3)
+		assert.Equals(t, invocations[0], "first")
+		assert.Equals(t, invocations[1], "second")
+		assert.Equals(t, invocations[2], "handler")
+	})
+
+	t.Run("when middleware does not call next it should short circuit the chain", func(t *testing.T) {
+		t.Parallel()
+
+		invocations := []string{}
+		mwList := []middleware.Middleware{
+			func(next http.HandlerFunc) http.HandlerFunc {
+				return func(writer http.ResponseWriter, request *http.Request) {
+					invocations = append(invocations, "first")
+					next(writer, request)
+				}
+			},
+			func(next http.HandlerFunc) http.HandlerFunc {
+				return func(writer http.ResponseWriter, request *http.Request) {
+					invocations = append(invocations, "second")
+				}
+			},
+		}
+		handler := func(w http.ResponseWriter, req *http.Request) {
+			invocations = append(invocations, "handler")
+		}
+		mwChain := middleware.CreateChain(mwList, handler)
+		mwChain.ServeHTTP(httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/", nil))
+
+		assert.Equals(t, len(invocations), 2)
+		assert.Equals(t, invocations[0], "first")
+		assert.Equals(t, invocations[1], "second")
+	})
 }
