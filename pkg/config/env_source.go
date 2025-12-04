3bdfbd1ab59a2cf772fd1108765ddb0468087117
David Ouellette <david.o@mailbox.org>
2025-12-04 14:21:55 -0800

    Fix bugs and add tests in various packages.

--- pkg/config/env_source.go
+++ pkg/config/env_source.go
@@ -3,7 +3,6 @@ package config
 import (
 	"os"
 
-	"github.com/TriangleSide/GoTools/pkg/stringcase"
 	"github.com/TriangleSide/GoTools/pkg/structs"
 )
 
@@ -15,7 +14,7 @@ const (
 // envSource fetches configuration values from environment variables. The variable name is derived from the
 // struct field name converted to SNAKE_CASE.
 func envSource(fieldName string, _ *structs.FieldMetadata) (string, bool, error) {
-	formattedEnvName := stringcase.CamelToSnake(fieldName)
+	formattedEnvName := camelToSnake(fieldName)
 	envValue, hasEnvValue := os.LookupEnv(formattedEnvName)
 	return envValue, hasEnvValue, nil
 }
