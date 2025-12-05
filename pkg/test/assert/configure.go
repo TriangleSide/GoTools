b0cbde185dcfbdfb442d14af3b6b8855ce28730e
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2025-12-04 20:45:46 -0800

    Remove unnecessary mutex from assert package.

--- pkg/test/assert/configure.go
+++ pkg/test/assert/configure.go
@@ -1,15 +1,7 @@
 package assert
 
-import "sync"
-
-var (
-	// testLocks holds a lock per test case.
-	testLocks = sync.Map{}
-)
-
 // Testing matches the functions on the testing.T struct.
 type Testing interface {
-	Name() string
 	Helper()
 	Error(...any)
 	Fatal(...any)
@@ -18,7 +10,6 @@ type Testing interface {
 // testContext implements the Testing interface. It can be configured using options.
 type testContext struct {
 	Testing
-	mu       *sync.Mutex
 	failFunc func(args ...any)
 }
 
@@ -34,12 +25,8 @@ func Continue() Option {
 
 // newTestContext creates a new testing struct with optional settings.
 func newTestContext(t Testing, options ...Option) *testContext {
-	muNotCast, _ := testLocks.LoadOrStore(t.Name(), &sync.Mutex{})
-	mu, _ := muNotCast.(*sync.Mutex)
-
 	newT := &testContext{
 		Testing:  t,
-		mu:       mu,
 		failFunc: t.Fatal,
 	}
 	for _, opt := range options {
@@ -51,7 +38,5 @@ func newTestContext(t Testing, options ...Option) *testContext {
 // fail calls either testing.T's Error or Fatal function.
 func (t *testContext) fail(args ...any) {
 	t.Helper()
-	t.mu.Lock()
-	defer t.mu.Unlock()
 	t.failFunc(args...)
 }
