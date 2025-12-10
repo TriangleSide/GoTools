c60b999c7d69a58770929d9421a53b3bc8c27a58
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2025-12-09 18:29:01 -0800

    Refactor jwt and logger tests to use individual test functions.

--- pkg/logger/config_test.go
+++ pkg/logger/config_test.go
@@ -12,63 +12,61 @@ import (
 	"github.com/TriangleSide/GoTools/pkg/test/assert"
 )
 
-func TestMustConfigure(t *testing.T) {
-	t.Run("when the config provider succeeds it sets the logger level", func(t *testing.T) {
-		t.Cleanup(func() {
-			logger.SetLevel(logger.LevelInfo)
-		})
+func TestMustConfigure_ConfigProviderSucceeds_SetsLoggerLevel(t *testing.T) {
+	t.Cleanup(func() {
+		logger.SetLevel(logger.LevelInfo)
+	})
+	logger.MustConfigure(logger.WithConfigProvider(func() (*logger.Config, error) {
+		return &logger.Config{
+			LogLevel: "debug",
+		}, nil
+	}))
+	assert.Equals(t, logger.GetLevel(), logger.LevelDebug)
+}
+
+func TestMustConfigure_IncorrectLevel_Panics(t *testing.T) {
+	assert.PanicExact(t, func() {
 		logger.MustConfigure(logger.WithConfigProvider(func() (*logger.Config, error) {
 			return &logger.Config{
-				LogLevel: "debug",
+				LogLevel: "incorrect",
 			}, nil
 		}))
-		assert.Equals(t, logger.GetLevel(), logger.LevelDebug)
-	})
+	}, "Failed to parse the log level (invalid log level: incorrect).")
+}
 
-	t.Run("when the level is incorrect it should panic", func(t *testing.T) {
-		assert.PanicExact(t, func() {
-			logger.MustConfigure(logger.WithConfigProvider(func() (*logger.Config, error) {
-				return &logger.Config{
-					LogLevel: "incorrect",
-				}, nil
-			}))
-		}, "Failed to parse the log level (invalid log level: incorrect).")
-	})
-
-	t.Run("when the config provider fails it should panic", func(t *testing.T) {
-		assert.PanicExact(t, func() {
-			logger.MustConfigure(logger.WithConfigProvider(func() (*logger.Config, error) {
-				return nil, errors.New("config error")
-			}))
-		}, "Failed to get logger config (config error).")
-	})
-
-	t.Run("when the output provider succeeds does not panic", func(t *testing.T) {
-		t.Cleanup(func() {
-			logger.SetOutput(os.Stdout)
-		})
-		var outputBuffer bytes.Buffer
-		logger.MustConfigure(logger.WithOutputProvider(func() (io.Writer, error) {
-			return &outputBuffer, nil
+func TestMustConfigure_ConfigProviderFails_Panics(t *testing.T) {
+	assert.PanicExact(t, func() {
+		logger.MustConfigure(logger.WithConfigProvider(func() (*logger.Config, error) {
+			return nil, errors.New("config error")
 		}))
-		logger.Error(context.Background(), "test message")
-		assert.Contains(t, outputBuffer.String(), "test message")
-	})
+	}, "Failed to get logger config (config error).")
+}
 
-	t.Run("when the output provider fails it should panic", func(t *testing.T) {
-		assert.PanicExact(t, func() {
-			logger.MustConfigure(logger.WithOutputProvider(func() (io.Writer, error) {
-				return nil, errors.New("output error")
-			}))
-		}, "Failed to get logger output (output error).")
+func TestMustConfigure_OutputProviderSucceeds_DoesNotPanic(t *testing.T) {
+	t.Cleanup(func() {
+		logger.SetOutput(os.Stdout)
 	})
+	var outputBuffer bytes.Buffer
+	logger.MustConfigure(logger.WithOutputProvider(func() (io.Writer, error) {
+		return &outputBuffer, nil
+	}))
+	logger.Error(context.Background(), "test message")
+	assert.Contains(t, outputBuffer.String(), "test message")
+}
 
-	t.Run("when the defaults are used it should set the defaults", func(t *testing.T) {
-		t.Cleanup(func() {
-			logger.SetLevel(logger.LevelInfo)
-		})
-		logger.SetLevel(logger.LevelTrace)
-		logger.MustConfigure()
-		assert.Equals(t, logger.GetLevel(), logger.LevelInfo)
+func TestMustConfigure_OutputProviderFails_Panics(t *testing.T) {
+	assert.PanicExact(t, func() {
+		logger.MustConfigure(logger.WithOutputProvider(func() (io.Writer, error) {
+			return nil, errors.New("output error")
+		}))
+	}, "Failed to get logger output (output error).")
+}
+
+func TestMustConfigure_DefaultsUsed_SetsDefaults(t *testing.T) {
+	t.Cleanup(func() {
+		logger.SetLevel(logger.LevelInfo)
 	})
+	logger.SetLevel(logger.LevelTrace)
+	logger.MustConfigure()
+	assert.Equals(t, logger.GetLevel(), logger.LevelInfo)
 }
