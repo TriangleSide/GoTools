7a13f756e272f62c9be736150e797219717fc2e9
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2025-12-11 06:06:38 -0800

    Add the previous persisted status to the Migrate function.

---
+++ pkg/database/migration/manager.go
@@ -0,0 +1,35 @@
+package migration
+
+import "context"
+
+// Manager defines the functions needed to manage and coordinate migrations.
+type Manager interface {
+	// AcquireDBLock must acquire a database wide lock.
+	// It is used in conjunction with EnsureDataStores and ReleaseDBLock.
+	AcquireDBLock(context.Context) error
+
+	// EnsureDataStores must ensure the migration data stores (collections, tables, ...) are created.
+	// There should be two data stores, one for the migration lock, and one for migration statuses.
+	EnsureDataStores(context.Context) error
+
+	// ReleaseDBLock must release the DB lock acquired by AcquireDBLock.
+	ReleaseDBLock(context.Context) error
+
+	// AcquireMigrationLock must acquire a migration lock.
+	// This is to ensure only one migrator can run at any given time.
+	AcquireMigrationLock(context.Context) error
+
+	// MigrationLockHeartbeat is called on a configurable frequency.
+	// It is meant to maintain the lock acquired with AcquireMigrationLock.
+	MigrationLockHeartbeat(context.Context) error
+
+	// ListStatuses returns data previously stored with PersistStatus.
+	ListStatuses(context.Context) ([]PersistedStatus, error)
+
+	// PersistStatus stores or overrides the status of a migration.
+	// Order must be unique in the data store.
+	PersistStatus(context.Context, Order, Status) error
+
+	// ReleaseMigrationLock must release the migration lock acquired with AcquireMigrationLock.
+	ReleaseMigrationLock(context.Context) error
+}
