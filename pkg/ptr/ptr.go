113ad75fb9ced74bba87d3aa7846fba9c976a296
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2025-12-17 13:18:04 -0800

    Rename error variable and simplify pointer creation.

--- pkg/ptr/ptr.go
+++ pkg/ptr/ptr.go
@@ -8,9 +8,7 @@ import (
 // This can be used as a utility to make pointers to static values.
 // For example: Of[uint](123) returns a uint pointer containing the value 123.
 func Of[T any](val T) *T {
-	valPtr := new(T)
-	*valPtr = val
-	return valPtr
+	return &val
 }
 
 // Is checks if the generic parameter is a pointer type.
