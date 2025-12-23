3ec3b4950f4e87543d0f509fb9ea73436bd98fa5
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2025-12-23 14:10:40 -0800

    Add ptr package docs.

---
+++ pkg/ptr/doc.go
@@ -0,0 +1,9 @@
+/*
+Package ptr provides utilities for working with pointers in Go.
+
+This package is useful when you need to create pointers to literal values or
+check whether a generic type parameter is a pointer type. A common use case
+is creating pointers to constants or literals for use with APIs that require
+pointer arguments.
+*/
+package ptr
