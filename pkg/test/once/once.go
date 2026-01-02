fd4a4c8400b6eb622875c39c9ad9306efc92bb1a
David Ouellette <david.o@mailbox.org>
2026-01-02 14:39:43 -0800

    Add once package for single-execution test callbacks.

---
+++ pkg/test/once/once.go
@@ -0,0 +1,21 @@
+package once
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+)
+
+var (
+	registeredSingleInvocations sync.Map
+)
+
+// Do executes the callback once per test or subtest.
+func Do(t *testing.T, callback func()) {
+	t.Helper()
+	_, file, lineNumber, _ := runtime.Caller(1)
+	uniqueID := fmt.Sprintf("%s-%d-%s", file, lineNumber, t.Name())
+	once, _ := registeredSingleInvocations.LoadOrStore(uniqueID, &sync.Once{})
+	once.(*sync.Once).Do(callback)
+}
