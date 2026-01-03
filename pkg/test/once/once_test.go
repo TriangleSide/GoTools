6c57f3efe1f54763e6e9966ad92c12112f2cc5e7
David Ouellette <114709555+TriangleSide@users.noreply.github.com>
2026-01-02 20:23:53 -0800

    Refactor tests to use once.Do instead of init functions.

--- pkg/test/once/once_test.go
+++ pkg/test/once/once_test.go
@@ -10,73 +10,93 @@ import (
 	"github.com/TriangleSide/GoTools/pkg/test/once"
 )
 
+var (
+	testDoSingleCallOnce        sync.Once
+	testDoMultipleCallsOnce     sync.Once
+	testDoConcurrentCallsOnce   sync.Once
+	testDoMultipleCallSitesOnce sync.Once
+	testDoDifferentSubtestsOnce sync.Once
+	testDoCallbackPanicsOnce    sync.Once
+)
+
 func TestDo_SingleCall_InvokesCallback(t *testing.T) {
 	t.Parallel()
-	var callCount atomic.Int32
-	once.Do(t, func() {
-		callCount.Add(1)
+	testDoSingleCallOnce.Do(func() {
+		var callCount atomic.Int32
+		once.Do(t, func() {
+			callCount.Add(1)
+		})
+		assert.Equals(t, int32(1), callCount.Load())
 	})
-	assert.Equals(t, int32(1), callCount.Load())
 }
 
 func TestDo_MultipleCalls_InvokesCallbackOnlyOnce(t *testing.T) {
 	t.Parallel()
-	var callCount atomic.Int32
-	for range 10 {
-		once.Do(t, func() { callCount.Add(1) })
-	}
-	assert.Equals(t, int32(1), callCount.Load())
+	testDoMultipleCallsOnce.Do(func() {
+		var callCount atomic.Int32
+		for range 10 {
+			once.Do(t, func() { callCount.Add(1) })
+		}
+		assert.Equals(t, int32(1), callCount.Load())
+	})
 }
 
 func TestDo_ConcurrentCalls_InvokesCallbackOnlyOnce(t *testing.T) {
 	t.Parallel()
-	const goroutines = 100
-	var callCount atomic.Int32
-	var waitGroup sync.WaitGroup
-	for range goroutines {
-		waitGroup.Go(func() {
-			once.Do(t, func() { callCount.Add(1) })
-		})
-	}
-	waitGroup.Wait()
-	assert.Equals(t, int32(1), callCount.Load())
+	testDoConcurrentCallsOnce.Do(func() {
+		const goroutines = 100
+		var callCount atomic.Int32
+		var waitGroup sync.WaitGroup
+		for range goroutines {
+			waitGroup.Go(func() {
+				once.Do(t, func() { callCount.Add(1) })
+			})
+		}
+		waitGroup.Wait()
+		assert.Equals(t, int32(1), callCount.Load())
+	})
 }
 
 func TestDo_MultipleCallSites_InvokesEachCallbackOnce(t *testing.T) {
 	t.Parallel()
-	var callCount1 atomic.Int32
-	var callCount2 atomic.Int32
-	var callCount3 atomic.Int32
-	once.Do(t, func() { callCount1.Add(1) })
-	once.Do(t, func() { callCount2.Add(1) })
-	once.Do(t, func() { callCount3.Add(1) })
-	assert.Equals(t, int32(1), callCount1.Load())
-	assert.Equals(t, int32(1), callCount2.Load())
-	assert.Equals(t, int32(1), callCount3.Load())
+	testDoMultipleCallSitesOnce.Do(func() {
+		var callCount1 atomic.Int32
+		var callCount2 atomic.Int32
+		var callCount3 atomic.Int32
+		once.Do(t, func() { callCount1.Add(1) })
+		once.Do(t, func() { callCount2.Add(1) })
+		once.Do(t, func() { callCount3.Add(1) })
+		assert.Equals(t, int32(1), callCount1.Load())
+		assert.Equals(t, int32(1), callCount2.Load())
+		assert.Equals(t, int32(1), callCount3.Load())
+	})
 }
 
 func TestDo_DifferentSubtests_InvokesCallbackForEach(t *testing.T) {
 	t.Parallel()
-	subtests := []struct {
-		name      string
-		callCount *atomic.Int32
-	}{
-		{name: "subtest1", callCount: &atomic.Int32{}},
-		{name: "subtest2", callCount: &atomic.Int32{}},
-		{name: "subtest3", callCount: &atomic.Int32{}},
-	}
-	for _, tc := range subtests {
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			once.Do(t, func() { tc.callCount.Add(1) })
-			assert.Equals(t, int32(1), tc.callCount.Load())
-		})
-	}
+	testDoDifferentSubtestsOnce.Do(func() {
+		subtests := []struct {
+			name      string
+			callCount *atomic.Int32
+		}{
+			{name: "subtest1", callCount: &atomic.Int32{}},
+			{name: "subtest2", callCount: &atomic.Int32{}},
+			{name: "subtest3", callCount: &atomic.Int32{}},
+		}
+		for _, tc := range subtests {
+			t.Run(tc.name, func(t *testing.T) {
+				once.Do(t, func() { tc.callCount.Add(1) })
+				assert.Equals(t, int32(1), tc.callCount.Load())
+			})
+		}
+	})
 }
 
 func TestDo_CallbackPanics_PropagatesPanic(t *testing.T) {
 	t.Parallel()
-	assert.PanicExact(t, func() {
-		once.Do(t, func() { panic(errors.New("callback panic")) })
-	}, "callback panic")
+	testDoCallbackPanicsOnce.Do(func() {
+		assert.PanicExact(t, func() {
+			once.Do(t, func() { panic(errors.New("callback panic")) })
+		}, "callback panic")
+	})
 }
